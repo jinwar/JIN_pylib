9b63071732633321ab2661953b0776fee75e204b
Ge Jin <jinwar@gmail.com>
2019-10-06 17:56:57 -0500

    old code

---
+++ BasicClass.py
@@ -0,0 +1,14 @@
+import pickle
+class BasicClass:
+
+	def save(self,filename):
+		with open(filename,'wb') as f:
+			pickle.dump(self,f)
+		print('saved the class to: ' + filename)
+
+	def load(self,filename):
+		# test if class data file exist
+		with open(filename,'rb') as f:
+			temp = pickle.load(f)
+		self.__dict__.update(temp.__dict__)
+
