1713a1cd2411750e5dacaf7729ff754c7df09598
Ge Jin <jinwar@gmail.com>
2021-02-07 07:20:23 -0700

    update frac hit picks

--- FracHitPick.py
+++ FracHitPick.py
@@ -6,6 +6,52 @@ from .VizUtil import CoPlot_Waterfall_Pumping
 import matplotlib.dates as mdates
 
 
+class PickFrac_nodraw(BasicClass):
+
+    def __init__(self,pickfile,fig):
+        self.pickfile = pickfile
+        self.fig = fig
+        try:
+            self.pickdf = pd.read_csv(pickfile)
+        except:
+            self.pickdf = pd.DataFrame(columns=['time','md'])
+        self.lines = []
+    
+    def draw(self):
+        plt.draw()
+        self.cid = self.fig.canvas.mpl_connect('key_press_event',self.event_handle)
+        self.update_lines()
+    
+    def update_lines(self):
+        try:
+            for l in self.lines:
+                l.pop(0).remove()
+        except:
+            pass
+        lines = []
+        edtime = self.Ddata.get_extent(use_timestamp=True)[1]
+        for i,row in self.pickdf.iterrows():
+            bgtime = pd.to_datetime(row['time'])
+            bgtime = mdates.date2num(bgtime)
+            lines.append(self.viz.axs[0].plot([bgtime,edtime],[row['md'],row['md']],'k--'))
+        self.lines = lines
+
+    
+    def event_handle(self,event):
+        if event.key == 'a':
+            self.pickdf.loc[len(self.pickdf),'time'] = mdates.num2date(event.xdata).strftime('%Y-%m-%d %H:%M:%S')
+            self.pickdf.loc[len(self.pickdf)-1,'md'] = event.ydata
+            self.update_lines()
+
+        if event.key == 'd':
+            mds = self.pickdf['md'].values.flatten()
+            mindist = np.min(np.abs(mds-event.ydata))
+            if mindist < 20:
+                ind = np.argmin(np.abs(mds-event.ydata))
+                self.pickdf = self.pickdf.drop(ind)
+                self.pickdf.reset_index(drop=True,inplace=True)
+            self.update_lines()
+
 
 class PickFrac(BasicClass):
 
