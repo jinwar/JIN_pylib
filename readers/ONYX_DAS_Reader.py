b477a3f627bec9c9fa3841312c4b1a87e1cf6b55
Ge Jin <jinwar@gmail.com>
2024-11-29 06:36:13 -0600

    update

--- readers/ONYX_DAS_Reader.py
+++ readers/ONYX_DAS_Reader.py
@@ -63,16 +63,19 @@ def create_spool(datapath,extension = '.h5'):
     files = glob(datapath+'/*'+extension)
     bgtimes = []
     edtimes = []
+    final_files = []
     print('Indexing Files....')
     for file in tqdm(files):
         try:
             bgt,edt = get_time_range(file)
             bgtimes.append(bgt)
             edtimes.append(edt)
+            final_files.append(file)
         except:
             print('Error reading file:',file)
+            continue
     
-    df = pd.DataFrame({'file':files,'start_time':bgtimes,'end_time':edtimes})
+    df = pd.DataFrame({'file':final_files,'start_time':bgtimes,'end_time':edtimes})
 
     sp = Spool.spool(df,reader, support_partial_reading=True)
     return sp
